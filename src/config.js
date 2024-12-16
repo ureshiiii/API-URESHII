75ac716107b0043b493766d6adbab1adf50102ed
Frhnnn <kiblatgt@gmail.com>
2024-12-16 21:12:48 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,15 +1,14 @@
 export default {
   // DB SQL
   dbConfig: {
-    host: process.env.DB_HOST || "sql12.freemysqlhosting.net",
-    user: process.env.DB_USER || "sql12748811",
-    password: process.env.DB_PASS || "Yy4ZX9LqDI",
-    database: process.env.DB_NAME || "sql12748811",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   },
   // GEMINI AI
   defaultModel: "gemini-1.5-flash-8b-latest",
   defaultLogic: "Kamu dibuat oleh farhan",
-  
   // SYSTEM
   API_KEY: process.env.API_KEY || "lovefirsha",
   allowedOrigins: ["https://www.ureshii.my.id", "https://ureshii.my.id", "https://api-ureshii.vercel.app", "https://api-ureshii.vercel.app/docs"]
