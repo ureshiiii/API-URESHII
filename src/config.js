77eb785bbb9dcb2ba5a7f5814d6c34c09ac82dd9
Frhnnn <kiblatgt@gmail.com>
2024-12-16 22:58:01 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ export default {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
   },
   // GEMINI AI
   defaultModel: "gemini-1.5-flash-8b-latest",
