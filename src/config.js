3f1275c58d34f0d5d69dc2018723fda691fd172b
Frhnnn <kiblatgt@gmail.com>
2024-12-17 20:17:07 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export default {
   // DB SQL
   dbConfig: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST || "sql12.freemysqlhosting.net",
+    user: process.env.DB_USER || "sql12752627",
+    password: process.env.DB_PASS || "9tivBBpSqD",
+    database: process.env.DB_NAME || "sql12752627",
+    port: process.env.DB_PORT || "3306",
   },
   // GEMINI AI
   defaultModel: "gemini-1.5-flash-8b-latest",
   defaultLogic: "Kamu dibuat oleh farhan",
   // SYSTEM
   API_KEY: process.env.API_KEY || "lovefirsha",
-  allowedOrigins: ["https://www.ureshii.my.id", "https://api.ureshii.my.id"]
+  allowedOrigins: ["https://www.ureshii.my.id", "https://ureshii.my.id", "https://api-ureshii.vercel.app", "https://api-ureshii.vercel.app/docs"]
 };
